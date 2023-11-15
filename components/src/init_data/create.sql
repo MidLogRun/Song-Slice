58067c346c52af0a581879c9a573bce2164e097b
Lilia Garcia <liga8260@colorado.edu>
2023-11-14 20:55:42 -0700

    added foreign keys and userLibrary table

--- components/src/init_data/create.sql
+++ components/src/init_data/create.sql
@@ -1,47 +1,44 @@
 CREATE TABLE user(
     user_id INT PRIMARY KEY,
-    username VARCHAR(50),
+    username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
-    email VARCHAR(50),
+    email VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE release(
     release_id INT PRIMARY KEY,
-    title varchar(50),
-    artist varchar(50),
-    release_year INT,
-    totalTracks INT,
-    overallRating DECIMAL CHECK (overallRating >= 1 and overallRating <=5)
+    title varchar(100) NOT NULL,
+    artist varchar(100) NOT NULL,
+    release_year INT NOT NULL,
+    totalTracks INT NOT NULL,
+    overallRating DECIMAL(3,2) -- Assuming ratings are on a scale of 0.00 to 5.00
 );
 
 CREATE TABLE song(
     song_id INT PRIMARY KEY,
-    songTitle VARCHAR(100),
-    duration_minutes INT,
-    duration_seconds INT
-);
-
-CREATE TABLE song_to_release(
-    song_id INT,
-    release_id INT
-);
-
-CREATE TABLE rateReview(
-    review_id INT PRIMARY KEY,
-    username_id INT,
     release_id INT,
-    title VARCHAR(50),
-    summary VARCHAR(300),
-    rating INT NOT NULL CHECK (rating >= 1 and rating <= 5)
+    songTitle VARCHAR(100) NOT NULL,
+    duration_minutes INT NOT NULL,
+    duration_seconds INT NOT NULL,
+    CONSTRAINT FK_song_release FOREIGN KEY (release_id) REFERENCES release(release_id)
 );
 
-CREATE TABLE review_to_release(
-    review_id INT,
-    release_id INT
-);
-
-CREATE TABLE review_to_user(
-    review_id INT,
+CREATE TABLE review(
+    review_id INT PRIMARY KEY,
     user_id INT,
-    isReviewed BOOLEAN(0)
+    release_id INT,
+    title VARCHAR(50) NOT NULL,
+    summary TEXT,
+    rating INT CHECK (rating >= 1 and rating <= 5) NOT NULL,
+    CONSTRAINT FK_review_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_review_release FOREIGN KEY (release_id) REFERENCES release(release_id),
+    CONSTRAINT UQ_user_release_review UNIQUE (user_id, release_id) -- Ensures that each user can only make one review per release
+);
+
+CREATE TABLE userLibrary(
+    user_id INT,
+    release_id INT,
+    CONSTRAINT FK_userLibrary_user FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT FK_userLibrary_release FOREIGN KEY (release_id) REFERENCES release(release_id),
+    CONSTRAINT PK_userLibrary PRIMARY KEY (user_id, release_id)
 );

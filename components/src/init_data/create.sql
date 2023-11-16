8e287a96c63996252c3a2f48ac4573b34c364338
Matthew Long <mattbasslong@gmail.com>
2023-11-15 21:45:45 -0700

    updated DB

--- components/src/init_data/create.sql
+++ components/src/init_data/create.sql
@@ -2,52 +2,49 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   username VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
   password VARCHAR(50) NOT NULL,
-  email VARCHAR(50)
+  email VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS release CASCADE;
 CREATE TABLE release(
     release_id INT PRIMARY KEY,
-    title varchar(50),
-    artist varchar(50),
-    release_year INT,
-    totalTracks INT,
-    overallRating DECIMAL CHECK (overallRating >= 1 and overallRating <=5)
+    title varchar(100) NOT NULL,
+    artist varchar(100) NOT NULL,
+    release_year INT NOT NULL,
+    totalTracks INT NOT NULL,
+    overallRating DECIMAL(3,2)
 );
 
 DROP TABLE IF EXISTS song CASCADE;
 CREATE TABLE song(
     song_id INT PRIMARY KEY,
-    songTitle VARCHAR(100),
-    duration_minutes INT,
-    duration_seconds INT
-);
-
-DROP TABLE IF EXISTS song_to_release CASCADE;
-CREATE TABLE song_to_release(
-    song_id INT,
-    release_id INT
-);
-
-DROP TABLE IF EXISTS rateReview CASCADE;
-CREATE TABLE rateReview(
-    review_id INT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
     release_id INT,
-    title VARCHAR(50),
-    summary VARCHAR(300),
-    rating INT NOT NULL CHECK (rating >= 1 and rating <= 5)
+    songTitle VARCHAR(100) NOT NULL,
+    duration_minutes INT NOT NULL,
+    duration_seconds INT NOT NULL,
+    CONSTRAINT FK_song_release FOREIGN KEY (release_id) REFERENCES release(release_id)
 );
 
-DROP TABLE IF EXISTS review_to_release CASCADE;
-CREATE TABLE review_to_release(
-    review_id INT,
-    release_id INT
+
+DROP TABLE IF EXISTS review CASCADE;
+CREATE TABLE review(
+    review_id INT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    release_id INT,
+    title VARCHAR(50) NOT NULL,
+    summary TEXT,
+    rating INT CHECK (rating >= 1 and rating <= 5) NOT NULL,
+    CONSTRAINT FK_review_user FOREIGN KEY (username) REFERENCES users(username),
+    CONSTRAINT FK_review_release FOREIGN KEY (release_id) REFERENCES release(release_id),
+    CONSTRAINT UQ_user_release_review UNIQUE (username, release_id) -- Ensures that each user can only make one review per release
 );
 
-DROP TABLE IF EXISTS review_to_user CASCADE;
-CREATE TABLE review_to_user(
-    review_id INT,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    isReviewed BIT(1)
+DROP TABLE IF EXISTS userLibrary CASCADE;
+CREATE TABLE userLibrary(
+    username VARCHAR(50) NOT NULL,
+    release_id INT,
+    CONSTRAINT FK_userLibrary_user FOREIGN KEY (username) REFERENCES users(username),
+    CONSTRAINT FK_userLibrary_release FOREIGN KEY (release_id) REFERENCES release(release_id),
+    CONSTRAINT PK_userLibrary PRIMARY KEY (username, release_id)
 );
+

3744fe908fdcedfd660bbb5d54b9916dea691f8c
Matthew Long <mattbasslong@gmail.com>
2023-11-15 21:14:11 -0700

    added drop tables for easier db recreation

--- components/src/init_data/create.sql
+++ components/src/init_data/create.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   username VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
   password VARCHAR(50) NOT NULL,
   email VARCHAR(50)
 );
 
-
+DROP TABLE IF EXISTS release CASCADE;
 CREATE TABLE release(
     release_id INT PRIMARY KEY,
     title varchar(50),
@@ -14,6 +15,7 @@ CREATE TABLE release(
     overallRating DECIMAL CHECK (overallRating >= 1 and overallRating <=5)
 );
 
+DROP TABLE IF EXISTS song CASCADE;
 CREATE TABLE song(
     song_id INT PRIMARY KEY,
     songTitle VARCHAR(100),
@@ -21,11 +23,13 @@ CREATE TABLE song(
     duration_seconds INT
 );
 
+DROP TABLE IF EXISTS song_to_release CASCADE;
 CREATE TABLE song_to_release(
     song_id INT,
     release_id INT
 );
 
+DROP TABLE IF EXISTS rateReview CASCADE;
 CREATE TABLE rateReview(
     review_id INT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
@@ -35,11 +39,13 @@ CREATE TABLE rateReview(
     rating INT NOT NULL CHECK (rating >= 1 and rating <= 5)
 );
 
+DROP TABLE IF EXISTS review_to_release CASCADE;
 CREATE TABLE review_to_release(
     review_id INT,
     release_id INT
 );
 
+DROP TABLE IF EXISTS review_to_user CASCADE;
 CREATE TABLE review_to_user(
     review_id INT,
     username VARCHAR(50) UNIQUE NOT NULL,
